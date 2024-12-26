4d62e6859f1737d279a82a6a1af151eedd1625f5
whereyoucanfindme <anishchy501625@gmail.com>
2024-12-26 20:46:20 +0545

    rapid fire django celery and redis

--- home/movies/tasks.py
+++ home/movies/tasks.py
@@ -2,4 +2,20 @@ from celery import shared_task
 
 @shared_task 
 def add(x, y): # do not use Django models instances as args , just json data 
+    # if I am using models 
+    """
+    from django.apps import apps 
+    MyModel = apps.get_model('app', "model")
+    """
     return x + y 
+
+@shared_task(name="multiply_two_numbers")
+def mul(x, y):
+    # Celery recognizes this as the 'multiple_two_numbers' task 
+    total = x * (y * random.randint(3, 100))
+    return total 
+
+@shared_task(name="sum_list_numbers")
+def xsum(numbers):
+    # Celery recognizes this as the 'sum_list_numbers' task 
+    return sum(numbers)
